a6dc965abdd8f295b4f8bf9fcbf84a4319d8e6eb
dewey preast <webwalkin@me.com>
2022-04-11 17:46:16 -0400

    adding some js starting to add the dynamic elements

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+let selCity = document.querySelector("#selCity");
+const key = "103bb6101110fbb5a22cbbc804eea90b";
+// var prevCityLoc = document.querySelector("");
+// var oneCall = "https://api.openweathermap.org/data/2.5/onecall?lat=${"lat"}&lon=${"lon"}&exclude={part}&appid={API key}";
+// let geoCode = https://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}appid=103bb6101110fbb5a22cbbc804eea90b);
+let dropDown = document.getElementById("dropdown-menu");
+let prevCityLoc = "";
+let setHome = "";
+let day1 = document.querySelector("day1");
+let day2 = document.querySelector("day2");
+let day3 = document.querySelector("day3");
+let day4 = document.querySelector("");
+let day5 = $("");
+
+
+
+
+function getCity(){
+    fetch("https://api.openweathermap.org/geo/1.0/direct?q=atlanta,ga,US&limit=1&appid=103bb6101110fbb5a22cbbc804eea90b");
+
+     
+    then (console.log)
+}

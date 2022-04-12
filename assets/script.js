a7fbaa9f2503664a56759900a8484ee52e6b5014
dewey preast <webwalkin@me.com>
2022-04-12 09:13:00 -0400

    working on elements in the javascript

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,28 @@
-let selCity = document.querySelector("#selCity");
-const key = "103bb6101110fbb5a22cbbc804eea90b";
+const moment = require("moment");
+
+var selCity = document.querySelector("#selCity");
+var baseUrl = "http://api.openweathermap.org"
+const apikey = "103bb6101110fbb5a22cbbc804eea90b";
 // var prevCityLoc = document.querySelector("");
 // var oneCall = "https://api.openweathermap.org/data/2.5/onecall?lat=${"lat"}&lon=${"lon"}&exclude={part}&appid={API key}";
-// let geoCode = https://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}appid=103bb6101110fbb5a22cbbc804eea90b);
-let dropDown = document.getElementById("dropdown-menu");
-let prevCityLoc = "";
-let setHome = "";
-let day1 = document.querySelector("day1");
-let day2 = document.querySelector("day2");
-let day3 = document.querySelector("day3");
-let day4 = document.querySelector("");
-let day5 = $("");
+// let geoCode = https://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}appid=);
+var dropDown = document.getElementById("dropdown-menu");
+var prevCityLoc = "";
+var setHome = "Atlanta,ga";
+var prevCityLocEl = document.queryselector("#previouslySearched`");
+var citySearchEl = document.querySelector("#city-search");
+var geo = '${baseUrl}/geo,${"selCity"},US&1,${"apikey"}';
+var day = document.querySelector("#weather-day-date");
+var temp = document.queryselector("#weather-day-temp")
+var humidity = document.queryselector("#weather-day-humidity")
+var wind = document.querySelector("#")
+let geoCode = (geo) => {
+    fetch(geo),
+    console.log
 
 
+    
+}
 
 
 function getCity(){
